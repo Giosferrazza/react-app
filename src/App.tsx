8603da6d4f4e45d218a59895e740f6573030ccc6
giosferrazza <giosferrazza12@gmail.com>
2026-07-14 22:42:54 -0700

    ReatTutorial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// 16:46
+import Message from "./message";
+
+function App() {
+  const name = ''
+  if (name)
+    return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>
+}
+
+export default App;
